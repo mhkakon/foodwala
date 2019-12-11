bd86588822b3f1211742d03374dbc7afacac05ef
kakon <mehedih.kakon@gmail.com>
2019-12-11 16:14:47 +0600

    loaded

---
+++ app/Http/Controllers/SeviceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\sevice;
+use Illuminate\Http\Request;
+
+class SeviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\sevice  $sevice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(sevice $sevice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\sevice  $sevice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(sevice $sevice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\sevice  $sevice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, sevice $sevice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\sevice  $sevice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(sevice $sevice)
+    {
+        //
+    }
+}
